389656acf7cee965194bb44e2417cc984bf3c9c3
Pat Losoponkul <patextreme@hotmail.com>
2017-10-28 19:33:36 +0700

    Add lazada review json file

---
+++ data/lazada/clean/mongo_import.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import pymongo as pmg
+from bson.objectid import ObjectId
+
+reviewDF = pd.read_csv('review.csv')
+reviewDF['time'] = pd.to_datetime(reviewDF['time'])
+docList = reviewDF.to_dict('records')
+
+
+URI = "mongodb://172.17.0.1:27017"
+client = pmg.MongoClient()
+
+for doc in docList:
+	doc['store_id'] = ObjectId("59f373a540e4c1960ce7c927")
+	client.openr.reviews.insert_one(doc)
